98978cbe0359949662253a1726ee624de40165d9
Kishan Prajapati <kishan.drupal@gmail.com>
2022-06-17 19:27:55 +0530

    node passport login

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router  = express.Router();
+//login page
+router.get('/', (req,res)=>{
+    res.render('welcome');
+})
+//register page
+router.get('/register', (req,res)=>{
+    res.render('register');
+})
+
+router.get('/dashboard',(req,res)=>{
+    res.render('dashboard',{user: req.user });
+    })
+
+module.exports = router; 
